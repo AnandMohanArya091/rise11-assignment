690e356412e23d7f215b536b44ce68d73a932a65
AnandMohanArya091 <anandarya00dbg@gmail.com>
2024-12-14 23:21:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import './index.css'
+import Dashboard from './pages/Dashboard';
+
+function App() {
+  return (
+    <>
+      <Dashboard />
+      {/* <h1 className='text-slate-200'>Hello</h1> */}
+    </>
+  );
+}
+
+export default App;
